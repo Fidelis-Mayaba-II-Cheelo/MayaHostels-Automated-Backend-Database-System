d81a9f6c67e114f986a6e5f8f8c04a6f2b4a363e
Fidelis-Mayaba-II-Cheelo <fidelismcheeloii@gmail.com>
2025-02-03 10:29:49 +0200

    Added a trigger for automatic welcome messages to new student system accounts

---
+++ Database Files/SQL files/Triggers/MayaHostels - WelcomeMessage.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+DROP TRIGGER IF EXISTS `tr_WelcomeNotificationAfterStudentRegistration` $$
+CREATE TRIGGER `tr_WelcomeNotificationAfterStudentRegistration` AFTER INSERT ON students FOR EACH ROW
+BEGIN
+DECLARE admin_id INT;
+DECLARE welcome_message VARCHAR(150);
+
+SELECT maya_hostels_admin_id INTO admin_id FROM `admin` WHERE username = 'admin';
+
+SET welcome_message = CONCAT("Welcome to Maya Hostels, ", NEW.student_name, "! We're thrilled to welcome you. We hope you have a wonderful stay.  For any assistance, please contact the hostel administration at 0963225635 or mayabacheelo@gmail.com.");
+
+INSERT INTO notifications(notification_message, maya_hostels_admin_id, maya_hostels_student_id) VALUES (welcome_message, admin_id, NEW.maya_hostels_student_id);
+
+END $$
+DELIMITER ;
