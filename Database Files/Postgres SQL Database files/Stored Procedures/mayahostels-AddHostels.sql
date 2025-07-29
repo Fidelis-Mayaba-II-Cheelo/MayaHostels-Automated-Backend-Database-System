ecaf0c987de928576f5b5c0dcbb0faaabf0d2101
Fidelis-Mayaba-II-Cheelo <fidelismcheeloii@gmail.com>
2025-07-29 22:25:56 +0200

    Few changes made to the BeforeDeleteHostel trigger to ensure it returns the OLD record instead of NULL.

--- Database Files/Postgres SQL Database files/Stored Procedures/mayahostels-AddHostels.sql
+++ Database Files/Postgres SQL Database files/Stored Procedures/mayahostels-AddHostels.sql
@@ -14,51 +14,54 @@ DECLARE
     v_number_of_bedspaces_per_room INT;
     v_hostel_accommodation_price_per_semester INT;
 BEGIN
-    -- Start transaction
-    BEGIN
-        -- Determine hostel capacity and bedspaces per room based on hostel type
-        CASE p_hostel_type
-            WHEN 'Single' THEN 
-                v_hostel_capacity := 1 * p_number_of_rooms;
-                v_number_of_bedspaces_per_room := 1;
-            WHEN 'Double' THEN 
-                v_hostel_capacity := 2 * p_number_of_rooms;
-                v_number_of_bedspaces_per_room := 2;
-            WHEN 'Triple' THEN 
-                v_hostel_capacity := 3 * p_number_of_rooms;
-                v_number_of_bedspaces_per_room := 3;
-            WHEN 'Quadruple' THEN 
-                v_hostel_capacity := 4 * p_number_of_rooms;
-                v_number_of_bedspaces_per_room := 4;
-            ELSE 
-                RAISE EXCEPTION 'Invalid hostel type';
-        END CASE;
+    -- Determine hostel capacity and bedspaces per room based on hostel type
+    CASE p_hostel_type
+        WHEN 'Single' THEN 
+            v_hostel_capacity := 1 * p_number_of_rooms;
+            v_number_of_bedspaces_per_room := 1;
+        WHEN 'Double' THEN 
+            v_hostel_capacity := 2 * p_number_of_rooms;
+            v_number_of_bedspaces_per_room := 2;
+        WHEN 'Triple' THEN 
+            v_hostel_capacity := 3 * p_number_of_rooms;
+            v_number_of_bedspaces_per_room := 3;
+        WHEN 'Quadruple' THEN 
+            v_hostel_capacity := 4 * p_number_of_rooms;
+            v_number_of_bedspaces_per_room := 4;
+        ELSE 
+            RAISE EXCEPTION 'Invalid hostel type';
+    END CASE;
 
-        -- Determine hostel accommodation price per semester
-        v_hostel_accommodation_price_per_semester := 
-            CASE p_semester
-                WHEN 1 THEN 4 * p_hostel_accommodation_price_per_month
-                WHEN 2 THEN 6 * p_hostel_accommodation_price_per_month
-                ELSE 0
-            END;
+    -- Determine hostel accommodation price per semester
+    v_hostel_accommodation_price_per_semester := 
+        CASE p_semester
+            WHEN 1 THEN 4 * p_hostel_accommodation_price_per_month
+            WHEN 2 THEN 6 * p_hostel_accommodation_price_per_month
+            ELSE 0
+        END;
 
-        -- Insert into the hostels table
-        INSERT INTO hostels(
-            hostel_name, number_of_rooms, hostel_status, hostel_type, hostel_capacity, 
-            number_of_bedspaces_per_room, hostel_accommodation_price_per_semester, 
-            hostel_accommodation_price_per_month, maya_hostels_admin_id
-        ) VALUES (
-            p_hostel_name, p_number_of_rooms, p_hostel_status, p_hostel_type, v_hostel_capacity, 
-            v_number_of_bedspaces_per_room, v_hostel_accommodation_price_per_semester, 
-            p_hostel_accommodation_price_per_month, p_maya_hostels_admin_id
-        );
+    IF v_number_of_bedspaces_per_room <= 0 THEN
+        RAISE EXCEPTION 'Number of bedspaces must be greater than 0';
+    END IF;
 
-        -- Commit transaction
-        COMMIT;
-    EXCEPTION
-        WHEN OTHERS THEN
-            -- Rollback transaction if an error occurs
-            ROLLBACK;
-            RAISE;
-    END;
+    IF v_hostel_capacity <= 0 THEN
+        RAISE EXCEPTION 'Please provide a valid hostel capacity';
+    END IF;
+
+    IF v_hostel_accommodation_price_per_semester <= 0 THEN
+        RAISE EXCEPTION 'Please provide a valid hostel accommodation price per semester. It must be greater than 0.';
+    END IF;
+
+    -- Insert into the hostels table
+    INSERT INTO hostels(
+        hostel_name, number_of_rooms, hostel_status, hostel_type, hostel_capacity, 
+        number_of_bedspaces_per_room, hostel_accommodation_price_per_semester, 
+        hostel_accommodation_price_per_month, maya_hostels_admin_id
+    ) VALUES (
+        p_hostel_name, p_number_of_rooms, p_hostel_status, p_hostel_type, v_hostel_capacity, 
+        v_number_of_bedspaces_per_room, v_hostel_accommodation_price_per_semester, 
+        p_hostel_accommodation_price_per_month, p_maya_hostels_admin_id
+    );
+
 END $$;
+
--- Database Files/Postgres SQL Database files/Triggers/MayaHostels - BeforeDeleteHostel.sql
+++ Database Files/Postgres SQL Database files/Triggers/MayaHostels - BeforeDeleteHostel.sql
@@ -5,13 +5,13 @@ AS $$
 DECLARE
 	mh_no_occupied_rooms INT;
 BEGIN
-SELECT SUM(COUNT(*)) INTO mh_no_occupied_rooms FROM students WHERE maya_hostels_hostel_id = OLD.maya_hostels_hostel_id;
+SELECT COUNT(*) INTO mh_no_occupied_rooms FROM students WHERE maya_hostels_hostel_id = OLD.maya_hostels_hostel_id;
 
 IF mh_no_occupied_rooms > 0 THEN
 	RAISE EXCEPTION 'Deletion of hostel with ID % cannot occur because it still has tenants', OLD.maya_hostels_hostel_id;
 END IF;
 
-RETURN NULL;
+RETURN OLD;
 END $$;
 CREATE OR REPLACE TRIGGER tr_BeforeDeleteHostel
 BEFORE DELETE ON hostels
